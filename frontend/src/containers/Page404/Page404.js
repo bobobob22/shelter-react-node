9702bc7d436ba1308f38cc4cb339c5f3607c08fb
konradhojda <konrad.hojda@gmail.com>
2019-02-06 14:52:50 +0100

    - add image to 404 page

--- frontend/src/containers/Page404/Page404.js
+++ frontend/src/containers/Page404/Page404.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom'
 import styles from './Page404.scss';
+import page404Img from '../../assets/images/imag.jpg'
 
 const page404 = ({location}) => (
     <div className={styles.Page404}>
@@ -8,6 +9,7 @@ const page404 = ({location}) => (
         <h2>No match found. Back to homepage<br/>
             <NavLink to="/">Homepage</NavLink>
         </h2>
+        <img src={page404Img} alt=""/>
     </div>
 );
 
