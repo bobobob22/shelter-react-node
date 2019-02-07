0fc51df75f45532f87d7c7e885049e493d272782
konradhojda <konrad.hojda@gmail.com>
2019-02-07 09:21:15 +0100

    - reorganize routing components, - reorganized variables in scss

---
+++ frontend/src/containers/Main/MainItem/MainItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Route, Switch} from "react-router-dom";
+import Home from "../../Home/Home";
+import Lost from "../../Lost/Lost";
+import AddNewPet from "../../AddNewPet/AddNewPet";
+import Page404 from "../../Page404/Page404";
+
+const mainItem = () => (
+    <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/lost" component={Lost}/>
+        <Route path="/add-new" component={AddNewPet}/>
+        <Route component={Page404}/>
+    </Switch>
+);
+
+export default mainItem;
--- frontend/src/containers/Navigation/Navigation.js
+++ frontend/src/containers/Navigation/Navigation.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
-import classes from './Navigation.scss';
-import {NavLink} from 'react-router-dom'
+import styles from './Navigation.scss';
 import NavigationItem from './NavigationItem/NavigationItem'
 
 
 class Navigation extends Component {
     render() {
         return (
-            <div className={classes.Navigation}>
-                <NavigationItem link="/" name="Wszystkie"/>
-                <NavigationItem link="/lost" name="Zaginione"/>
-                <NavigationItem link="/add-new" name="Add New"/>
+            <div className={styles.Navigation}>
+                <div className={styles.navigation__left}>
+                    <NavigationItem link="/" name="Wszystkie"/>
+                    <NavigationItem link="/lost" name="Zaginione"/>
+                    <NavigationItem link="/lost" name="Do adopcji"/>
+                    <NavigationItem link="/lost" name="Znalezione"/>
+                    <NavigationItem link="/lost" name="Schroniska"/>
+                    <NavigationItem link="/add-new" name="Nowe ogłoszenie"/>
+                </div>
+                <div className={styles.Navigation__right}>
+                    <NavigationItem link="/" name="Wszystkie"/>
+                    <NavigationItem link="/lost" name="Zaginione"/>
+                    <NavigationItem link="/lost" name="Do adopcji"/>
+                    <NavigationItem link="/lost" name="Znalezione"/>
+                    <NavigationItem link="/lost" name="Schroniska"/>
+                    <NavigationItem link="/add-new" name="Nowe ogłoszenie"/>
+                </div>
             </div>
         );
     }
