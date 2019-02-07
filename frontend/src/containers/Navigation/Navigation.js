5453e9c3224d243933e3ab7ec1e02303277344bd
konradhojda <konrad.hojda@gmail.com>
2019-02-07 10:34:03 +0100

    - change url's to add new pet

--- frontend/src/containers/Navigation/Navigation.js
+++ frontend/src/containers/Navigation/Navigation.js
@@ -6,22 +6,21 @@ import NavigationItem from './NavigationItem/NavigationItem'
 class Navigation extends Component {
     render() {
         return (
-            <div className={styles.Navigation}>
+            <div className={styles.navigation}>
                 <div className={styles.navigation__left}>
                     <NavigationItem link="/" name="Wszystkie"/>
                     <NavigationItem link="/lost" name="Zaginione"/>
-                    <NavigationItem link="/lost" name="Do adopcji"/>
-                    <NavigationItem link="/lost" name="Znalezione"/>
-                    <NavigationItem link="/lost" name="Schroniska"/>
-                    <NavigationItem link="/add-new" name="Nowe ogłoszenie"/>
+                    <NavigationItem link="/" name="Do adopcji"/>
+                    <NavigationItem link="/" name="Znalezione"/>
+                    <NavigationItem link="/" name="Schroniska"/>
                 </div>
-                <div className={styles.Navigation__right}>
-                    <NavigationItem link="/" name="Wszystkie"/>
-                    <NavigationItem link="/lost" name="Zaginione"/>
-                    <NavigationItem link="/lost" name="Do adopcji"/>
-                    <NavigationItem link="/lost" name="Znalezione"/>
-                    <NavigationItem link="/lost" name="Schroniska"/>
-                    <NavigationItem link="/add-new" name="Nowe ogłoszenie"/>
+                <div className={styles.navigation__right}>
+                    <NavigationItem link="/" name="Ulubione"/>
+                    <NavigationItem link="/" name="Twoje ogłoszenia"/>
+                    <NavigationItem link="/" name="Wsparcie"/>
+                    <NavigationItem link="/addnotice" name="Dodaj ogłoszenie"/>
+                    <NavigationItem link="/" name="Zarejestruj się"/>
+                    <NavigationItem link="/" name="Zaloguj się"/>
                 </div>
             </div>
         );
