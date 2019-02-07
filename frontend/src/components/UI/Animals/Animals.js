dccec42fca7b4dd20702074018ac46764bf3e470
konradhojda <konrad.hojda@gmail.com>
2019-02-07 13:59:05 +0100

    - fetch data from server (i've made json to show this)

---
+++ frontend/src/components/UI/Animals/Animals.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './Animals.scss';
+import Button from "../Button/Button";
+
+
+const animals = (props) => (
+    <>
+        <div className={styles.pet}>
+            <div className={styles.pet__img}>
+                <img src={props.img} alt=""/>
+            </div>
+            <div className={styles.pet__wrapper}>
+                <div className={styles.pet__info}>
+                    <h2>
+                        <span>{props.name}</span>
+                        <span>{props.race}</span>
+                    </h2>
+                    <p>{props.place}</p>
+                    <p className={styles.pet__description}>{props.description}</p>
+                </div>
+                <div className={styles.pet__buttons}>
+                    <Button>{props.destination}</Button>
+                    <Button>{props.destination}</Button>
+                    <Button>{props.gender}</Button>
+                </div>
+            </div>
+        </div>
+        <hr/>
+    </>
+);
+
+
+export default animals;
