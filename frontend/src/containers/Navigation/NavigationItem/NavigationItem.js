09313cd00760cc64bf2acb4ec99e54d234fd08cc
konradhojda <konrad.hojda@gmail.com>
2019-02-12 15:51:47 +0100

    - fix navigation problem with displaying link

--- frontend/src/containers/Navigation/NavigationItem/NavigationItem.js
+++ frontend/src/containers/Navigation/NavigationItem/NavigationItem.js
@@ -7,8 +7,10 @@ import classNames from 'classnames';
 const navigationItem = (props) => (
     <ul className={styles.navigation__elementsContainer}>
         <li className={styles.navigation__item}>
-            <FontAwesomeIcon icon={props.icon} className={classNames(styles.nav__icon, [props.className])}/>
-            <NavLink to={props.link} className={styles.navigation__link}>{props.name}</NavLink>
+            <NavLink to={props.link} className={styles.navigation__link}>
+                <FontAwesomeIcon icon={props.icon} className={classNames(styles.nav__icon, [props.className])}/>
+                {props.name}
+            </NavLink>
         </li>
     </ul>
 );
