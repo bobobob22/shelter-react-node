a60c74c24b8c58450c00621c7cd668256c1e3d02
konradhojda <konrad.hojda@gmail.com>
2019-02-12 15:33:33 +0100

    - add icons to nav (by props)

--- frontend/src/containers/Navigation/NavigationItem/NavigationItem.js
+++ frontend/src/containers/Navigation/NavigationItem/NavigationItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import styles from './NavigationItem.scss'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import classNames from 'classnames';
 
 const navigationItem = (props) => (
-    <ul className={styles.navigation}>
+    <ul className={styles.navigation__elementsContainer}>
         <li className={styles.navigation__item}>
+            <FontAwesomeIcon icon={props.icon} className={classNames(styles.nav__icon, [props.className])}/>
             <NavLink to={props.link} className={styles.navigation__link}>{props.name}</NavLink>
         </li>
     </ul>
